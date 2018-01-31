89afabe45811a838a37137fa98b4514bce412334
Omkar Bolla <omkar062@gmail.com>
2018-01-31 10:13:14 +0530

    changes in app

--- sig_test.c
+++ sig_test.c
@@ -12,6 +12,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define SIG_NUM_H	61
 #define SIG_NUM_R	62
@@ -21,7 +22,8 @@
 #if 1
 static int fd_cnt;
 char file_path[PATH_MAX];
-#define DIR_PATH	"/mnt/extra";
+char fd_path[PATH_MAX];
+#define DIR_PATH	"/mnt/extra"
 #if 0
 if (fcntl(fd, F_GETPATH, filePath) != -1)
 {
@@ -30,18 +32,44 @@ if (fcntl(fd, F_GETPATH, filePath) != -1)
 int fstat(int fd, struct stat *buf);
 #endif
 /* implementation of Donal Fellows method */ 
+#if 0
+void print_type(char *type, struct pid_info_t* info)
+{
+    static ssize_t link_dest_size;
+    static char link_dest[PATH_MAX];
+    strncat(info->path, type, sizeof(info->path));
+    if ((link_dest_size = readlink(info->path, link_dest, sizeof(link_dest)-1)) < 0) {
+        if (errno == ENOENT)
+            goto out;
+        snprintf(link_dest, sizeof(link_dest), "%s (readlink: %s)", info->path, strerror(errno));
+    } else {
+        link_dest[link_dest_size] = '\0';
+    }
+    // Things that are just the root filesystem are uninteresting (we already know)
+    if (!strcmp(link_dest, "/"))
+        goto out;
+    printf("%-9s %5d %10s %4s %9s %18s %9s %10s %s\n",
+            info->cmdline, info->pid, info->user, type,
+            "???", "???", "???", "???", link_dest);
+out:
+    info->path[info->parent_length] = '\0';
+}
+#endif
 int get_num_fds()
 {
-     char buf[64];
+//     char buf[64];
      struct dirent *d;
      DIR *dp = NULL;
 
 printf("FD_SETSIZE: %d\n", FD_SETSIZE);
-     snprintf(buf, 64, "/proc/%i/fd/", getpid());
+     snprintf(fd_path, 64, "/proc/%i/fd/", getpid());
 
      fd_cnt = 0;
-     dp = opendir(buf);
+     dp = opendir(fd_path);
      while ((d = readdir(dp)) != NULL) {
+printf("%s\n", d->d_name);
+if (!strcmp(d->d_name, ".") || !strcmp(d->d_name, ".."))
+                continue;
           fd_cnt++;
      }
      closedir(dp);
@@ -52,16 +80,31 @@ printf("FD_SETSIZE: %d\n", FD_SETSIZE);
 void sig_handler_h(int sig)
 {
 int i;
+int ret = 0;
 	printf("%s(): signal number: %d\n", __func__, sig);
 get_num_fds();
 	printf("fd_cnt: %d\n", fd_cnt);	
-
+#if 1
 for (i=0;i < fd_cnt; i++) {
-if (fcntl(i, F_GETPATH, file_path) != -1) {
-if (!strncmp(file_path, DIR_PATH, strlen(DIR_PATH)))
-	printf("fd: %d, PATH: %s\n", i, file_path);
+//strcat(fd_path, );
+snprintf(fd_path, PATH_MAX, "/proc/%i/fd/%i", getpid(),i);
+printf("fd_path: %s\n", fd_path);
+
+ret = readlink(fd_path, file_path, sizeof(file_path)-1);
+if (ret < 0) {
+if (errno != ENOENT)
+perror("readlink");
+break;
+} else {
+file_path[ret] = '\0';
+printf("fd: %d, PATH: %s\n", i, file_path);
+}
+
+if (!strncmp(file_path, DIR_PATH, strlen(DIR_PATH))) {
+printf("fd: %d, ONLY PATH: %s\n", i, file_path);
 }
 }
+#endif
 
 }
 void sig_handler_r(int sig)
@@ -79,6 +122,7 @@ int main(int argc, char **argv)
 	int ret = 0;
 	int debug = 0;
 
+	printf("%s PID: %d\n", argv[0], getpid());
 if (argc > 2) {
 	printf("<%s> <1>\n", argv[0]);
 	return -1;
